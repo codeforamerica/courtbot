6b1aa144da82dc5c325dda92c55f2c3a06c991f0
Sam Hashemi <sam@samhashemi.com>
2014-03-16 15:44:51 -0700

    Tweaked postgres support and added twilio package.

--- web.js
+++ web.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var pg = require('pg');
 var app = express();
 
 app.use(logfmt.requestLogger());
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  if (err) return console.error(err);
+
+  client.query('SELECT * FROM your_table', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+});
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
