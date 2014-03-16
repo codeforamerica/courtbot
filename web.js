31c2cabc7a419d519b432efe8d489b724bccbd8d
Sam Hashemi <sam@samhashemi.com>
2014-03-16 15:54:46 -0700

    Trying out Twilio integration.

--- web.js
+++ web.js
@@ -3,6 +3,9 @@ var logfmt = require("logfmt");
 var pg = require('pg');
 var app = express();
 
+//require the Twilio module and create a REST client
+var client = require('twilio')('PN4f8d200af39a91f20272f96a5ba8b050', 'ACa1a2f0c274fa21513d4fa48b243bd14c');
+
 app.use(logfmt.requestLogger());
 
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
@@ -18,6 +21,14 @@ pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
+
+// Respond to text messages that come in from Twilio
+app.post('/sms', function(req, res) {
+  var twiml = new twilio.TwimlResponse();
+  twiml.sms('hello. i am courtbot. some call me the bot of justice.')
+  res.send(twiml.toString());
+});
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
