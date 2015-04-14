83ec587df4a4c3f00017af5b56503d911032a2fd
Stephen Bussey <steve.bussey@salesloft.com>
2015-04-13 20:47:14 -0400

    test exact matching capability of /cases

--- test/web_test.js
+++ test/web_test.js
@@ -39,4 +39,49 @@ describe("GET /cases", function() {
       .get('/cases')
       .expect(400, done);
   });
+
+  it("200s + empty array when there is ?q=", function(done) {
+    request(app)
+      .get('/cases?q=test')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.text).to.equal("[]");
+        done();
+      });
+  });
+
+  it("finds exact matches", function(done) {
+    knex('cases').del().then(function() {
+      knex('cases').insert([turnerData(1), turnerData(2)]).then(function() {
+        request(app)
+          .get('/cases?q=turner')
+          .expect(200)
+          .end(function(err, res) {
+            if (err) return done(err);
+            expect(JSON.parse(res.text)).to.deep.equal([turnerDataAsObject(1), turnerDataAsObject(2)]);
+            done();
+          });
+      });
+    });
+  });
 });
+
+function turnerData(v) {
+  return { date: '27-MAR-15',
+    defendant: 'TURNER, FREDERICK T',
+    room: 'CNVCRT',
+    time: '01:00:00 PM',
+    citations: '[{"id":"4928456","violation":"40-8-76.1","description":"SAFETY BELT VIOLATION","location":"27 DECAATUR ST","payable":"1"}]',
+    id: '677167760f89d6f6ddf7ed19ccb63c15486a0eab' + (v||0)
+  };
+}
+
+function turnerDataAsObject(v) {
+  var data = turnerData(v);
+  data.date = "2015-03-27T00:00:00.000Z";
+  data.citations = JSON.parse(data.citations);
+  data.payable = true;
+  data.readableDate = "Thursday, Mar 26th";
+  return data;
+}
