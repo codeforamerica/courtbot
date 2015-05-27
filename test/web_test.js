87d81a1125844f58ab0f448da2b6aa5f8c924199
Stephen Bussey <steve.bussey@salesloft.com>
2015-05-26 21:03:08 -0400

    being testing /sms. Only tested basic logic paths

--- test/web_test.js
+++ test/web_test.js
@@ -8,8 +8,21 @@ var tk = require('timekeeper');
 var fs = require('fs');
 var Promise = require('bluebird');
 var moment = require("moment");
-var request = require('supertest');
-var app = require('../web');
+var _ = require("underscore");
+var cookieParser = require("cookie-parser");
+var Session = require('supertest-session')({
+  app: require('../web')
+});
+
+var sess;
+
+beforeEach(function () {
+  sess = new Session();
+});
+
+afterEach(function () {
+  sess.destroy();
+});
 
 var Knex = require('knex');
 var knex = Knex.initialize({
@@ -19,13 +32,20 @@ var knex = Knex.initialize({
 
 nock.enableNetConnect('127.0.0.1');
 
+function getConnectCookie() {
+  var sessionCookie = _.find(sess.cookies, function(cookie) {
+    return _.has(cookie, 'connect.sess');
+  });
+  var cookie = sessionCookie['connect.sess'];
+  return cookieParser.JSONCookie(cookieParser.signedCookie(cookie, process.env.COOKIE_SECRET));
+}
+
 describe("GET /", function() {
   it("responds with a simple message", function(done) {
-    request(app)
-      .get('/')
-      .expect('Content-Length', '79')
-      .expect(200)
-      .end(function(err, res) {
+    sess.get('/').
+      expect('Content-Length', '79').
+      expect(200).
+      end(function(err, res) {
         if (err) return done(err);
         expect(res.text).to.contain("Hello, I am Courtbot.");
         done();
@@ -35,16 +55,14 @@ describe("GET /", function() {
 
 describe("GET /cases", function() {
   it("400s when there is no ?q=", function(done) {
-    request(app)
-      .get('/cases')
-      .expect(400, done);
+    sess.get('/cases').
+      expect(400, done);
   });
 
   it("200s + empty array when there is ?q=", function(done) {
-    request(app)
-      .get('/cases?q=test')
-      .expect(200)
-      .end(function(err, res) {
+    sess.get('/cases?q=test').
+      expect(200).
+      end(function(err, res) {
         if (err) return done(err);
         expect(res.text).to.equal("[]");
         done();
@@ -54,10 +72,9 @@ describe("GET /cases", function() {
   it("finds partial matches of name", function(done) {
     knex('cases').del().then(function() {
       knex('cases').insert([turnerData(1), turnerData(2)]).then(function() {
-        request(app)
-          .get('/cases?q=turner')
-          .expect(200)
-          .end(function(err, res) {
+        sess.get('/cases?q=turner').
+          expect(200).
+          end(function(err, res) {
             if (err) return done(err);
             expect(JSON.parse(res.text)).to.deep.equal([turnerDataAsObject(1), turnerDataAsObject(2)]);
             done();
@@ -69,10 +86,9 @@ describe("GET /cases", function() {
   it("finds exact matches of id", function(done) {
     knex('cases').del().then(function() {
       knex('cases').insert([turnerData()]).then(function() {
-        request(app)
-          .get('/cases?q=4928456')
-          .expect(200)
-          .end(function(err, res) {
+        sess.get('/cases?q=4928456').
+          expect(200).
+          end(function(err, res) {
             if (err) return done(err);
             expect(JSON.parse(res.text)).to.deep.equal([turnerDataAsObject()]);
             done();
@@ -84,16 +100,98 @@ describe("GET /cases", function() {
   it("doesnt find partial matches of id", function(done) {
     knex('cases').del().then(function() {
       knex('cases').insert([turnerData()]).then(function() {
-        request(app)
-          .get('/cases?q=492845')
-          .expect(200)
-          .end(function(err, res) {
+        sess.get('/cases?q=492845').
+          expect(200).
+          end(function(err, res) {
             if (err) return done(err);
             expect(JSON.parse(res.text)).to.deep.equal([]);
             done();
           });
       });
     });
+  });
+});
+
+describe("POST /sms", function() {
+  beforeEach(function(done) {
+    knex('cases').del().then(function() {
+      knex('cases').insert([turnerData()]).then(function() {
+        done();
+      });
+    });
+  });
+
+  context("without session set", function() {
+    context("with 1 matching court case", function() {
+      var params = "4928456";
+
+      context("it can pay online", function() {
+
+      });
+
+      context("it can not be paid online", function() {
+
+      });
+    });
+
+    context("with 0 matching court cases", function() {
+      context("with a citation length between 6-9 inclusive", function() {
+        var params = { Body: "123456" };
+
+        it("says we couldn't find their case and prompt for reminder", function(done) {
+          sess.
+            post('/sms').
+            send(params).
+            expect(200).
+            end(function(err, res) {
+              if (err) { return done(err); }
+              expect(res.text).to.equal('<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Couldn&apos;t find your case. It takes 14 days for new citations to appear in the sytem. Would you like a text when we find your information? (Reply YES or NO)</Sms></Response>');
+              done();
+            });
+        });
+
+        it("sets the askedQueued and citationId cookies", function(done) {
+          sess.
+            post('/sms').
+            send(params).
+            expect(200).
+            end(function(err, res) {
+              if (err) { return done(err); }
+              expect(getConnectCookie().askedQueued).to.equal(true);
+              expect(getConnectCookie().askedReminder).to.equal(undefined);
+              expect(getConnectCookie().citationId).to.equal("123456");
+              done();
+            });
+        });
+      });
+
+      context("the citation length is too long", function() {
+        var params = { Body: "123456789123456" };
+
+        it("says that you need to call", function(done) {
+          sess.
+            post('/sms').
+            send(params).
+            expect(200).
+            end(function(err, res) {
+              if (err) { return done(err); }
+              expect(res.text).to.equal('<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Sorry, we couldn&apos;t find that court case. Please call us at (404) 954-7914.</Sms></Response>');
+              expect(getConnectCookie().askedQueued).to.equal(undefined);
+              expect(getConnectCookie().askedReminder).to.equal(undefined);
+              expect(getConnectCookie().citationId).to.equal(undefined);
+              done();
+          });
+        });
+      });
+    });
+  });
+
+  context("with session.askedReminder", function() {
+
+  });
+
+  context("with session.askedQueued", function() {
+
   });
 });
 
