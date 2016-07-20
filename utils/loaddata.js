2847f0d62813ea252b27afa508c08310dcc1ed15
Tim Escher <tim@eschers.com>
2016-07-19 21:30:36 -0700

    PSQL Timezone

--- utils/loaddata.js
+++ utils/loaddata.js
@@ -13,7 +13,12 @@ var knex = Knex.initialize({
   connection: process.env.DATABASE_URL,
   pool: {
     min: 0,
-    max: 7
+    max: 7,
+    afterCreate: function(connection, callback) {
+      connection.query("SET TIME ZONE 'America/Anchorage';", function(err) {
+        callback(err, connection);
+      });
+    }
   }
 
 });
