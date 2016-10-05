f021a0aad6dee3ef5e9b0c9cee32e38e8eb9fcaf
Nigel Kibodeaux <nigel@team.about.me>
2016-10-04 20:07:24 -0800

    temporarily comment out dotenv thin for load data

--- utils/loaddata.js
+++ utils/loaddata.js
@@ -6,7 +6,7 @@ var request = require('request');
 var parse = require('csv-parse');
 var Promise = require('bluebird');
 var sha1 = require('sha1');
-require('dotenv').config();
+// require('dotenv').config();
 
 var Knex = require('knex');
 var knex = Knex.initialize({
