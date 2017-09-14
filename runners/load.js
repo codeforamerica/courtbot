a341e5bba0653a4237a18862e04387f38c4bcfd2
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-09-13 16:34:38 -0800

    use rollbar, structure like other runners

--- runners/load.js
+++ runners/load.js
@@ -1,9 +1,19 @@
-var loadScript = require("../utils/loaddata");
+require('dotenv').config();  // needed for local dev when not using Heroku to pull in env vars
+var runnerScript = require("../utils/loaddata.js");
+var rollbar = require("rollbar");
 
-loadScript().then(function(success) {
-  console.log(success);
+rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN);
+
+runnerScript().then(function(success) {
+  console.log("Success: ",success);
   process.exit(0);
 }, function(err) {
-  console.log(err);
-  process.exit(1);
+  console.log("Error: ",err);
+  // Using callback for process.exit() so the process does not exit before rollbar
+  //    is finished sending error.
+  //    Sending null as second arg since there is no
+  //    request object
+  rollbar.handleError(err, null, function() {
+      process.exit(1);
+  });
 });
