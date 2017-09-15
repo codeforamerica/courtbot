c8e2199de39c72d24a9886d24c6779ac832463b8
Mark Meyer <mark@photo-mark.com>
2017-09-15 13:35:44 -0800

    replace whereRaw querries with where to allow knex to handle user input. Clean up indenting

--- db.js
+++ db.js
@@ -1,88 +1,78 @@
-var crypto = require('crypto');
 require('dotenv').config();
-var manager = require("./utils/db/manager");
-var knex = manager.knex();
-var now = require("./utils/dates").now;
+
+const crypto = require('crypto');
+const manager = require("./utils/db/manager");
+const knex = manager.knex();
+const now = require("./utils/dates").now;
 
 exports.findCitation = function(citation) {
-  // Postgres JSON search based on prebuilt index
-  citation = escapeSQL(citation.toUpperCase().trim());
-  //var citationSearch = knex.raw(`'{"${citation}"}'::text[] <@ (json_val_arr(citations, 'id'))`);
-  var citationSearch = knex.raw(`citations @> '[{"id": "${citation}"}]' `);
-  return knex('cases').where(citationSearch).select();
-};
+    const cleaned_citation = escapeSQL(citation.toUpperCase().trim());
+    return knex('cases').where('citations', '@>', `[{"id": "${cleaned_citation}"}]`).select();
+ };
 
 // Find queued citations that we have asked about adding reminders
 exports.findAskedQueued = function(phone) {
-  var cipher = crypto.createCipher('aes256', process.env.PHONE_ENCRYPTION_KEY);
-  var encryptedPhone = cipher.update(phone, 'utf8', 'hex') + cipher.final('hex');
+    const cipher = crypto.createCipher('aes256', process.env.PHONE_ENCRYPTION_KEY);
+    const encryptedPhone = cipher.update(phone, 'utf8', 'hex') + cipher.final('hex');
 
-  // Filter for new ones. If too old, user probably missed the message (same timeframe as Twilio sessions - 4 hours). Return IFF one found. If > 1 found, skip
-  return knex('queued')
-    .where('phone',encryptedPhone).andWhere('asked_reminder',true)
-    .andWhereRaw(`"asked_reminder_at" > current_timestamp - interval '4 hours'`)
-    .select()
-    .then(function(rows) {
-      if (rows.length == 1) {
-        //var citationSearch = knex.raw(`'{"${rows[0].citation_id}"}'::text[] <@ (json_val_arr(citations, 'id'))`);
-
-        var citationSearch = knex.raw(`citations @> '[{"id": "${rows[0].citation_id}"}]' `);
-        return knex('queued')
-          .where('queued_id', rows[0].queued_id)
-          .update({'asked_reminder':false})
-          .then(function(values) {
-            return knex('cases').where(citationSearch).select()
-          })
-      } else {
-        return  [];
-      }
-    })
+    // Filter for new ones. If too old, user probably missed the message (same timeframe as Twilio sessions - 4 hours). Return IFF one found. If > 1 found, skip
+    return knex('queued')
+        .where('phone',encryptedPhone).andWhere('asked_reminder',true)
+        .andWhereRaw(`"asked_reminder_at" > current_timestamp - interval '4 hours'`)
+        .select()
+        .then((rows) => {
+            if (rows.length == 1) {
+                return knex('queued')
+                .where('queued_id', rows[0].queued_id)
+                .update({'asked_reminder':false})
+                .then((values) => knex('cases').where('citations', '@>', `[{"id": "${rows[0].citation_id}"}]`))
+            } else {
+                return  [];
+            }
+        })
 };
 
 exports.fuzzySearch = function(str) {
-  str = str.trim();
-  var parts = str.toUpperCase().split(" ");
+    const parts = str.trim().toUpperCase().split(" ");
 
-  // Search for Names
-  var query = knex('cases').where('defendant', 'ilike', '%' + parts[0] + '%');
-  if (parts.length > 1) query = query.andWhere('defendant', 'ilike', '%' + parts[1] + '%');
+    // Search for Names
+    let query = knex('cases').where('defendant', 'ilike', '%' + parts[0] + '%');
+    if (parts.length > 1) query = query.andWhere('defendant', 'ilike', '%' + parts[1] + '%');
 
-  // Search for Citations
-  var citation = escapeSQL(parts[0]);
- // var citationSearch = knex.raw(`'{"${citation}"}'::text[] <@ (json_val_arr(citations, 'id'))`);
-  var citationSearch = knex.raw(`citations @> '[{"id": "${citation}"}]' `);
-  query = query.orWhere(citationSearch);
+    // Search for Citations
+    const cleaned_citation = escapeSQL(parts[0]);
+    query = query.orWhere('citations', '@>', `[{"id": "${cleaned_citation}"}]`);
 
-  // Limit to ten results
-  query = query.limit(10);
-  return query;
+    // Limit to ten results
+    query = query.limit(10);
+    return query;
 };
 
 exports.addReminder = function(data) {
-  var cipher = crypto.createCipher('aes256', process.env.PHONE_ENCRYPTION_KEY);
-  var encryptedPhone = cipher.update(data.phone, 'utf8', 'hex') + cipher.final('hex');
+    const cipher = crypto.createCipher('aes256', process.env.PHONE_ENCRYPTION_KEY);
+    const encryptedPhone = cipher.update(data.phone, 'utf8', 'hex') + cipher.final('hex');
 
-  return knex('reminders').insert({
-    case_id: data.caseId,
-    sent: false,
-    phone: encryptedPhone,
-    created_at: now(),
-    original_case: data.originalCase,
-  })
+    return knex('reminders').insert({
+        case_id: data.caseId,
+        sent: false,
+        phone: encryptedPhone,
+        created_at: now(),
+        original_case: data.originalCase,
+    })
 };
 
 exports.addQueued = function(data) {
-  var cipher = crypto.createCipher('aes256', process.env.PHONE_ENCRYPTION_KEY);
-  var encryptedPhone = cipher.update(data.phone, 'utf8', 'hex') + cipher.final('hex');
+    const cipher = crypto.createCipher('aes256', process.env.PHONE_ENCRYPTION_KEY);
+    const encryptedPhone = cipher.update(data.phone, 'utf8', 'hex') + cipher.final('hex');
 
-  return knex('queued').insert({
-    citation_id: data.citationId,
-    sent: false,
-    phone: encryptedPhone,
-    created_at: now()
-  });
+    return knex('queued').insert({
+        citation_id: data.citationId,
+        sent: false,
+        phone: encryptedPhone,
+        created_at: now()
+    });
 };
 
 var escapeSQL = function(val) {
-  return val.replace(/[^A-Za-z0-9\-]/g, "")
+    return val.replace(/[^A-Za-z0-9\-]/g, "")
 };
