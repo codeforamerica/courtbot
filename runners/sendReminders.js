c75dbf0bf6dc3e500bbb295ec4bd33d72fac7caf
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-09-14 22:10:32 -0800

    eslint placations

--- runners/sendReminders.js
+++ runners/sendReminders.js
@@ -1,18 +1,21 @@
-require('dotenv').config();  // needed for local dev when not using Heroku to pull in env vars
-var runnerScript = require("../sendReminders.js").sendReminders;
-var rollbar = require("rollbar");
+/* eslint "no-console": "off" */
+
+require('dotenv').config(); // needed for local dev when not using Heroku to pull in env vars
+const runnerScript = require('../sendReminders.js').sendReminders;
+const rollbar = require('rollbar');
+
 rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN);
 
-runnerScript().then(function(success) {
-  console.log("Success: ",success);
+runnerScript().then((success) => {
+  console.log('Success: ', success);
   process.exit(0);
-}, function(err) {
-  console.log("Error: ",err);
+}, (err) => {
+  console.log('Error: ', err);
   // Using callback for process.exit() so the process does not exit before rollbar
   //    is finished sending error.
   //    Sending null as second arg since there is no
   //    request object
-  rollbar.handleError(err, null, function() {
-      process.exit(1);
+  rollbar.handleError(err, null, () => {
+    process.exit(1);
   });
 });
