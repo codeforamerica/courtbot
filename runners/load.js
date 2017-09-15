ab4e2388d654be9d7f9f7fff7c136f03fe65afce
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-09-14 23:44:13 -0800

    eslint placations, use catch

--- runners/load.js
+++ runners/load.js
@@ -1,19 +1,23 @@
-require('dotenv').config();  // needed for local dev when not using Heroku to pull in env vars
-var runnerScript = require("../utils/loaddata.js");
-var rollbar = require("rollbar");
+/* eslint "no-console": "off" */
+
+require('dotenv').config(); // needed for local dev when not using Heroku to pull in env vars
+const runnerScript = require('../utils/loaddata.js');
+const rollbar = require('rollbar');
 
 rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN);
 
-runnerScript().then(function(success) {
-  console.log("Success: ",success);
-  process.exit(0);
-}, function(err) {
-  console.log("Error: ",err);
-  // Using callback for process.exit() so the process does not exit before rollbar
-  //    is finished sending error.
-  //    Sending null as second arg since there is no
-  //    request object
-  rollbar.handleError(err, null, function() {
+runnerScript()
+  .then((success) => {
+    console.log('Success: ', success);
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.log('Error: ', err);
+    // Using callback for process.exit() so the process does not exit before rollbar
+    //    is finished sending error.
+    //    Sending null as second arg since there is no
+    //    request object
+    rollbar.handleError(err, null, () => {
       process.exit(1);
+    });
   });
-});
