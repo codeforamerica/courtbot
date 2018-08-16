6ebcd7effd0fa80cdf1b4d35487ad421e7a5dfc0
Marvin Frederickson <marvin.frederickson@gmail.com>
2018-08-16 09:35:31 -0800

    remove noconsole eslint directive

--- runners/load.js
+++ runners/load.js
@@ -1,5 +1,3 @@
-/* eslint "no-console": "off" */
-
 require('dotenv').config(); // needed for local dev when not using Heroku to pull in env vars
 const runnerScript = require('../utils/loaddata.js');
 const manager = require('../utils/db/manager')
@@ -8,14 +6,6 @@ const log = require('../utils/logger')
 
 runnerScript()
 .then((r) => runner_log.loaded(r))
-.then(() => {
-    log.debug('checking active handles')
-    let v = process._getActiveHandles()
-    log.debug(v)
-    log.debug('checking active requests')
-    let u = process._getActiveRequests()
-    log.debug(u)
-})
 .then(() => manager.knex.destroy())
 .catch((err) => {
     manager.knex.destroy()
