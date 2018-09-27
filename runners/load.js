e7d6412935e182f9f702fb5e5571a03730b4e5cc
Mark Meyer <mark@photo-mark.com>
2018-09-26 17:39:07 -0800

    Retry load script on five minute intervals on failure

--- runners/load.js
+++ runners/load.js
@@ -4,10 +4,23 @@ const manager = require('../utils/db/manager')
 const runner_log = require('../utils/logger/runner_log')
 const log = require('../utils/logger')
 
-runnerScript()
-.then((r) => runner_log.loaded(r))
-.then(() => manager.knex.destroy())
-.catch((err) => {
-    manager.knex.destroy()
-    log.error(err)
-});
+let count = 0
+const max_tries = 6
+const time_between_retries = 5 * 60 * 1000
+
+function load(){
+    count++
+    runnerScript()
+    .then((r) => runner_log.loaded(r))
+    .then(() => manager.knex.destroy())
+    .catch((err) => {
+        if (count < max_tries){
+            setTimeout(load, time_between_retries) 
+        } else {
+            manager.knex.destroy()
+            log.error(err)    
+        }
+    });    
+}
+
+load()
