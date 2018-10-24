4e41cef7e06cb706ffbc47ae31836675a4b38afa
Mark Meyer <mark@photo-mark.com>
2018-10-24 15:21:05 -0800

    Adds a module to manage a test case. The test case number should be set in .env. If no test case number is set, this should do nothing

--- runners/sendReminders.js
+++ runners/sendReminders.js
@@ -5,9 +5,12 @@ const runnerScript = require('../sendReminders.js').sendReminders;
 const manager = require('../utils/db/manager')
 const runner_log = require('../utils/logger/runner_log')
 const log = require('../utils/logger')
+const {deleteTestRequests, incrementTestCaseDate} = require('../utils/testCase')
 
 runnerScript()
 .then(reminders => runner_log.sent({action: 'send_reminder', data: reminders}))
+.then(() => deleteTestRequests())
+.then(() => incrementTestCaseDate())
 .then(() => manager.knex.destroy())
 .catch((err) => {
     manager.knex.destroy()
